0632ee5630e07c568714cea6b14338947aecb2e8
Mohamed Ouedarbi <mouedarbi@gmail.com>
2026-04-27 19:29:17 +0200

    Refonte Etape 1 : Isolation du projet, création de la Home Page en dur et stratégie de migration hors-Wagtail.

--- app/marketplace/settings.py
+++ app/marketplace/settings.py
@@ -150,7 +150,7 @@ AUTH_USER_MODEL = 'accounts.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_refonte.sqlite3',
     }
 }
 
