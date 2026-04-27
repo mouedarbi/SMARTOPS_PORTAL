3c69c993529d5cb2c6dcf0cf1d8cfd7c843c75f2
Mohamed Ouedarbi <mouedarbi@gmail.com>
2026-04-27 17:25:50 +0200

    Prep deployment: Add gunicorn, whitenoise, and env example. Ensure DB and Media are tracked.

--- app/marketplace/settings.py
+++ app/marketplace/settings.py
@@ -99,6 +99,7 @@ AUTHENTICATION_BACKENDS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -214,3 +215,12 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
